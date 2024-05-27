207c082cc235d76b953bc0007c2cf41353d8114b
kaarthikapashikanti <pashikantikaarthika2004@gmail.com>
2024-05-27 16:39:19 +0530

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,639 @@
+import basket_icon from "./basket_icon.png";
+import logo from "./real1_logo.png";
+import header_img from "./header_img.png";
+import search_icon from "./search_icon.png";
+import menu_1 from "./real_pureveg.jpg";
+import menu_2 from "./real_nonveg.jpg";
+import menu_3 from "./real_panner.jpg";
+import menu_4 from "./real_chicken.jpg";
+import menu_5 from "./real_tikka.jpg";
+import menu_6 from "./real_tandoori.jpg";
+import menu_7 from "./real_italy.jpg";
+import menu_8 from "./real_greek.jpg";
+import menu_9 from "./real_stuffedcrust.jpg";
+
+import food_1 from "./marghertiapizza.jpg";
+import food_2 from "./schezwanmarghertiapizza.jpg";
+import food_3 from "./veggiefeastpizza.jpg";
+import food_4 from "./cornandcheessepizza.jpg";
+import food_5 from "./classiconioncapsicumpizza.jpg";
+import food_6 from "./classiconionpizza.jpg";
+import food_7 from "./dhabedakeemapizza.jpg";
+import food_8 from "./nonvegloadedpizza.jpg";
+import food_9 from "./muttonkeemapizza.jpg";
+import food_10 from "./pannermazedarmakhnipizza.jpg";
+import food_11 from "./pannerspicedpizza.jpg";
+import food_12 from "./pannerchillisizzlepizza.jpg";
+import food_13 from "./pannerperiperipizza.jpg";
+import food_14 from "./chickensizzlingschezwanpizza.jpg";
+import food_15 from "./awesomeamericanchessychickenpizza.jpg";
+import food_16 from "./chickensupremepizza.jpg";
+import food_17 from "./chickensausagepizza.jpg";
+import food_18 from "./chickenpepperonipizzza.jpg";
+import food_19 from "./classicchickenpepperoniandonionpizza.jpg";
+import food_20 from "./pannertikka.jpg";
+import food_21 from "./chickentikka.jpg";
+import food_22 from "./tawachickentikka.jpg";
+import food_23 from "./indichickentikka.jpg";
+import food_24 from "./ultimatetandooriveggie.jpg";
+import food_25 from "./tandooripanner.jpeg";
+import food_26 from "./inditandoori.jpg";
+import food_27 from "./chickentandoori.jpg";
+import food_28 from "./napoetanapizza.jpg";
+import food_29 from "./altagliopizza.jpg";
+import food_30 from "./sicilianapizza.jpg";
+import food_31 from "./fetacheesepizza.jpg";
+import food_32 from "./greekpizza.jpg";
+import food_33 from "./newyorkstylepizza.jpg";
+import food_34 from "./chicagostylepizza.jpg";
+import toppings1 from "./toppings1.jpg";
+import toppings2 from "./toppings2.jpg";
+import toppings3 from "./toppings3.jpg";
+import toppings4 from "./toppings4.jpg";
+import toppings5 from "./toppings5.jpg";
+import toppings6 from "./toppings6.jpg";
+import toppings7 from "./toppings7.jpg";
+import toppings8 from "./toppings8.jpg";
+import toppings9 from "./toppings9.jpg";
+import toppings10 from "./toppings10.jpg";
+import toppings11 from "./toppings11.jpg";
+import toppings12 from "./toppings12.jpg";
+import toppings13 from "./toppings13.jpg";
+import toppings14 from "./toppings14.jpg";
+import toppings15 from "./toppings15.jpg";
+import toppings16 from "./toppings16.jpg";
+import toppings17 from "./toppings17.jpg";
+import toppings18 from "./toppings18.jpg";
+import toppings19 from "./toppings19.jpg";
+import toppings20 from "./toppings20.jpg";
+import toppings21 from "./toppings21.jpg";
+import toppings22 from "./toppings22.jpg";
+import toppings23 from "./toppings23.jpg";
+import toppings24 from "./toppings24.jpg";
+import add_icon_white from "./add_icon_white.png";
+import add_icon_green from "./add_icon_green.png";
+import remove_icon_red from "./remove_icon_red.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import linkedin_icon from "./linkedin_icon.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import cross_icon from "./cross_icon.png";
+import selector_icon from "./selector_icon.png";
+import rating_starts from "./rating_starts.png";
+import profile_icon from "./profile_icon.png";
+import bag_icon from "./bag_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icon from "./parcel_icon.png";
+import ownpizza from "./ownpizza.png";
+import pwddot from "./passworddot.jpg";
+import pwdopen from "./passwordopen.png";
+
+export const assets = {
+  logo,
+  basket_icon,
+  header_img,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  bag_icon,
+  parcel_icon,
+  pwddot,
+  pwdopen,
+};
+
+export const base_list = [
+  {
+    _id: "1",
+    name: "Yeasted",
+    image: toppings1,
+    price: 120,
+  },
+  {
+    _id: "2",
+    name: "FlatBread",
+    image: toppings2,
+    price: 150,
+  },
+  {
+    _id: "3",
+    name: "classic HandTossed",
+    image: toppings3,
+    price: 200,
+  },
+  {
+    _id: "4",
+    name: "Thin crust",
+    image: toppings4,
+    price: 220,
+  },
+  {
+    _id: "5",
+    name: "Cheese-StuffedCrust",
+    image: toppings5,
+    price: 300,
+  },
+];
+export const sauce_list = [
+  {
+    _id: "6",
+    name: "Spicy-Red Sauce",
+    image: toppings6,
+    price: 50,
+  },
+  {
+    _id: "7",
+    name: "Red Sauce",
+    image: toppings7,
+    price: 40,
+  },
+  {
+    _id: "8",
+    name: "Sweet Sauce",
+    image: toppings8,
+    price: 80,
+  },
+  {
+    _id: "9",
+    name: "Chocolate Sauce",
+    image: toppings9,
+    price: 60,
+  },
+  {
+    _id: "10",
+    name: "BBQ Sauce",
+    image: toppings10,
+    price: 100,
+  },
+];
+export const cheese_list = [
+  {
+    _id: "11",
+    name: "Mozzarella Cheese",
+    image: toppings11,
+    price: 100,
+  },
+  {
+    _id: "12",
+    name: "Provolone Cheese",
+    image: toppings12,
+    price: 120,
+  },
+  {
+    _id: "13",
+    name: "Cheddar Cheese",
+    image: toppings13,
+    price: 110,
+  },
+  {
+    _id: "14",
+    name: "Feta Cheese",
+    image: toppings14,
+    price: 120,
+  },
+  {
+    _id: "15",
+    name: "Goat Cheese",
+    image: toppings15,
+    price: 200,
+  },
+];
+export const veggies_list = [
+  {
+    _id: "16",
+    name: "Plain",
+    image: toppings16,
+    price: 0,
+  },
+  {
+    _id: "17",
+    name: "Onions",
+    image: toppings17,
+    price: 50,
+  },
+  {
+    _id: "18",
+    name: "Cherry Tomatoes",
+    image: toppings18,
+    price: 90,
+  },
+  {
+    _id: "19",
+    name: "Olives",
+    image: toppings19,
+    price: 100,
+  },
+  {
+    _id: "20",
+    name: "Mushroom",
+    image: toppings20,
+    price: 150,
+  },
+  {
+    _id: "21",
+    name: "Baby Spinach",
+    image: toppings21,
+    price: 90,
+  },
+  {
+    _id: "22",
+    name: "Capsicum",
+    image: toppings22,
+    price: 80,
+  },
+  {
+    _id: "23",
+    name: "corn",
+    image: toppings23,
+    price: 70,
+  },
+  {
+    _id: "24",
+    name: "Optional Garnishes",
+    image: toppings24,
+    price: 100,
+  },
+];
+export let food_list = [
+  {
+    _id: "1",
+    name: "Customize Your Pizza",
+    image: ownpizza,
+    price: "",
+    description: "Customize your own pizza with your own pulses",
+    category: [],
+  },
+  {
+    _id: "2",
+    name: "Marghertia",
+    image: food_1,
+    price: 350,
+    description:
+      "A Margherita pizza is a classic pizza that is celebrated for its simplicity and rich flavors",
+    category: ["Pure-Veg"],
+  },
+  {
+    _id: "3",
+    name: "Schezwan Margherita",
+    image: food_2,
+    price: 400,
+    description:
+      "The Schezwan Margherita is a fusion pizza combining the classic simplicity of a Margherita with the bold, spicy flavors of Schezwan sauce.",
+    category: ["Pure-Veg"],
+  },
+  {
+    _id: "4",
+    name: "Veggie Feast",
+    image: food_3,
+    price: 380,
+    description:
+      "The Veggie Feast pizza is a vibrant and wholesome option loaded with an assortment of fresh vegetables",
+    category: ["Pure-Veg"],
+  },
+  {
+    _id: "5",
+    name: "corn and Cheese",
+    image: food_4,
+    price: 350,
+    description:
+      "The Corn and Cheese pizza is a delightful combination featuring a creamy blend of melted cheese and sweet, juicy corn kernels.",
+    category: ["Pure-Veg"],
+  },
+  {
+    _id: "6",
+    name: "Classic onion capsicum",
+    image: food_5,
+    price: 300,
+    description:
+      "The Classic Onion Capsicum pizza is a simple yet flavorful choice, featuring a base of tangy tomato sauce topped with generous slices of crisp onions and fresh capsicum",
+    category: ["Pure-veg"],
+  },
+  {
+    _id: "7",
+    name: "Classic Onion",
+    image: food_6,
+    price: 250,
+    description:
+      "The Classic Onion pizza is a straightforward and tasty option, featuring a tangy tomato sauce base topped with layers of thinly sliced, sweet onions.",
+    category: ["Pure-Veg"],
+  },
+  {
+    _id: "8",
+    name: "Dhabe Da Keema",
+    image: food_7,
+    price: 400,
+    description:
+      "The Dhabe Da Keema pizza is a flavorful fusion dish inspired by traditional Indian roadside dhaba cuisine. It features a rich and spicy minced meat (keema) topping",
+    category: ["Non-Veg"],
+  },
+  {
+    _id: "9",
+    name: "Non-veg loaded",
+    image: food_8,
+    price: 420,
+    description:
+      "The Non-Veg Loaded pizza is a hearty and indulgent option packed with a variety of meats. It typically includes toppings like pepperoni, sausage, ham, chicken, and bacon",
+    category: ["Non-Veg"],
+  },
+  {
+    _id: "10",
+    name: "Mutton keema",
+    image: food_9,
+    price: 500,
+    description:
+      "The Mutton Keema pizza offers a delicious twist with its flavorful minced mutton topping, seasoned with aromatic spices and herbs",
+    category: ["Non-Veg"],
+  },
+  {
+    _id: "11",
+    name: "Mazedar Makhni Panner",
+    image: food_10,
+    price: 370,
+    description:
+      "The Mazedar Makhni Paneer pizza is a delightful fusion of Indian and Italian flavors. It features creamy and tangy Makhni  sauce as the base, topped with chunks of soft paneer",
+    category: ["Pure-Veg", "Panner"],
+  },
+  {
+    _id: "12",
+    name: "Spiced Panner",
+    image: food_11,
+    price: 390,
+    description:
+      "The Spiced Paneer pizza offers a burst of Indian flavors with its marinated and grilled paneer cubes, seasoned with aromatic spices like cumin, coriander, and chili powder",
+    category: ["Pure-Veg", "Panner"],
+  },
+  {
+    _id: "13",
+    name: "Chilli Panner sizzle",
+    image: food_12,
+    price: 350,
+    description:
+      "The Chilli Paneer Sizzle pizza is a fiery and sizzling delight, featuring chunks of spicy chili paneer on a bed of tangy tomato sauce",
+    category: ["Pure-veg", "Panner"],
+  },
+  {
+    _id: "14",
+    name: "Peri-Peri Panner",
+    image: food_13,
+    price: 350,
+    description:
+      "The Peri-Peri Paneer pizza is a zesty and flavorful option, showcasing marinated paneer cubes infused with the bold and spicy Peri-Peri seasoning",
+    category: ["Pure-Veg", "Panner"],
+  },
+  {
+    _id: "15",
+    name: "Sizzling schezwan Chicken",
+    image: food_14,
+    price: 420,
+    description:
+      "The Sizzling Schezwan Chicken pizza is a fiery and savory delight, featuring tender chicken pieces tossed in spicy Schezwan sauce",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "16",
+    name: "Awesome American chessy",
+    image: food_15,
+    price: 380,
+    description:
+      "The Awesome American Cheesy pizza is a decadent treat loaded with a generous blend of various cheeses, such as mozzarella, cheddar, and parmesan",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "17",
+    name: "Chicken Supreme",
+    image: food_16,
+    price: 370,
+    description:
+      "The Chicken Supreme pizza is a hearty and flavorful choice, packed with tender chicken pieces, savory mushrooms, crisp bell peppers, and onions",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "18",
+    name: "Chicken Sausage",
+    image: food_17,
+    price: 400,
+    description:
+      "The Chicken Sausage pizza is a savory delight, featuring slices of flavorful chicken sausage atop a tangy tomato sauce base.",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "19",
+    name: "chicken Pepperoni",
+    image: food_18,
+    price: 390,
+    description:
+      "The Chicken Pepperoni pizza is a delicious twist on a classic favorite, featuring spicy slices of chicken pepperoni on a tangy tomato sauce base",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "20",
+    name: "Classic chicken pepperoni and onion",
+    image: food_19,
+    price: 350,
+    description:
+      "The Classic Chicken Pepperoni and Onion pizza is a flavorful combination, featuring spicy slices of chicken pepperoni and crisp onions atop a tangy tomato sauce base",
+    category: ["Non-Veg", "Chicken"],
+  },
+  {
+    _id: "21",
+    name: "Panner tikka",
+    image: food_20,
+    price: 400,
+    description:
+      "The Paneer Tikka pizza is a delightful fusion dish that features chunks of marinated paneer, grilled to perfection with aromatic spices",
+    category: ["Pure-Veg", "Panner", "Tikka"],
+  },
+  {
+    _id: "22",
+    name: "Chicken tikka",
+    image: food_21,
+    price: 430,
+    description:
+      "The Chicken Tikka pizza offers a flavorful twist with tender pieces of marinated chicken tikka, infused with aromatic Indian spices",
+    category: ["Non-Veg", "Chicken", "Tikka"],
+  },
+  {
+    _id: "23",
+    name: "Tawa chicken tikka",
+    image: food_22,
+    price: 420,
+    description:
+      "The Tawa Chicken Tikka pizza brings the authentic flavors of Indian street food to your plate, featuring tender chunks of chicken tikka cooked on a traditional tawa (griddle).",
+    category: ["Non-Veg", "Chicken", "Tikka"],
+  },
+  {
+    _id: "24",
+    name: "Indi Chicken tikka",
+    image: food_23,
+    price: 400,
+    description:
+      "The Indi Chicken Tikka pizza is a delicious fusion of Indian flavors and Italian cuisine. It features tender pieces of marinated chicken tikka, infused with aromatic spices",
+    category: ["Non-Veg", "Chicken", "Tikka"],
+  },
+  {
+    _id: "25",
+    name: "Ultimate tandoori veggie",
+    image: food_24,
+    price: 360,
+    description:
+      "The Ultimate Tandoori Veggie pizza is a flavor-packed delight, featuring a medley of vibrant vegetables marinated in aromatic tandoori spices",
+    category: ["Pure-Veg", "Tandoori"],
+  },
+  {
+    _id: "26",
+    name: "Tandoori Panner",
+    image: food_25,
+    price: 380,
+    description:
+      "The Tandoori Paneer pizza offers a delightful blend of Indian flavors and Italian comfort. It showcases tender chunks of paneer",
+    category: ["Pure-Veg", "Panner", "Tandoori"],
+  },
+  {
+    _id: "27",
+    name: "Indi tandoori",
+    image: food_26,
+    price: 320,
+    description:
+      "The Indi Tandoori pizza is a celebration of vibrant Indian flavors on a classic Italian canvas. It features succulent pieces of chicken tikka, paneer, or a combination of both",
+    category: ["Non-Veg", "Tandoori"],
+  },
+  {
+    _id: "28",
+    name: "Chicken tandoori",
+    image: food_27,
+    price: 430,
+    description:
+      "The Chicken Tandoori pizza is a mouthwatering fusion of Indian and Italian cuisines. It boasts tender pieces of chicken marinated in traditional tandoori spices",
+    category: ["Non-Veg", "Chicken", "Tandoori"],
+  },
+  {
+    _id: "29",
+    name: "Napoletana",
+    image: food_28,
+    price: 550,
+    description:
+      "The Napoletana pizza is a classic Italian dish, featuring a thin crust topped with simple yet flavorful ingredients.",
+    category: ["Pure-Veg", "Italian"],
+  },
+  {
+    _id: "30",
+    name: "Al-Taglio",
+    image: food_29,
+    price: 550,
+    description:
+      "Al Taglio pizza, also known as Roman-style pizza, is a rectangular-shaped pizza originating from Rome, Italy",
+    category: ["Pure-Veg", "Italian"],
+  },
+  {
+    _id: "31",
+    name: "Siciliana",
+    image: food_30,
+    price: 500,
+    description:
+      "The Siciliana pizza, hailing from the Italian island of Sicily, is renowned for its bold and robust flavors. This pizza typically features a thick and fluffy crust",
+    category: ["Non-Veg", "Italian"],
+  },
+  {
+    _id: "32",
+    name: "Feta Cheese",
+    image: food_31,
+    price: 490,
+    description:
+      "Feta Cheese pizza is a delightful option for cheese enthusiasts seeking a tangy and savory flavor profile.",
+    category: ["Pure-Veg", "Greek"],
+  },
+  {
+    _id: "33",
+    name: "Greek",
+    image: food_32,
+    price: 470,
+    description:
+      " The Greek pizza is a Mediterranean-inspired delight, featuring flavors reminiscent of the coastal regions of Greece. ",
+    category: ["Pure-Veg", "Greek"],
+  },
+  {
+    _id: "34",
+    name: "NewYork-style",
+    image: food_33,
+    price: 530,
+    description:
+      "The New York-style pizza is an iconic American classic, known for its large, thin, and foldable slices. Characterized by a hand-tossed crust that's crispy on the outside",
+    category: ["Non-Veg", "StuffedCrust"],
+  },
+  {
+    _id: "35",
+    name: "Chicago style",
+    image: food_34,
+    price: 580,
+    description:
+      "Chicago-style pizza is a hearty and indulgent dish originating from the Windy City. Unlike its thin-crust New York counterpart",
+    category: ["Pure-Veg", "StuffedCrust"],
+  },
+];
+export const menu_list = [
+  {
+    menu_id: 1,
+    menu_name: "Pure-Veg",
+    menu_image: menu_1,
+  },
+  {
+    menu_id: 2,
+    menu_name: "Non-Veg",
+    menu_image: menu_2,
+  },
+  {
+    menu_id: 3,
+    menu_name: "Panner",
+    menu_image: menu_3,
+  },
+  {
+    menu_id: 4,
+    menu_name: "Chicken",
+    menu_image: menu_4,
+  },
+  {
+    menu_id: 5,
+    menu_name: "Tikka",
+    menu_image: menu_5,
+  },
+  {
+    menu_id: 6,
+    menu_name: "Tandoori",
+    menu_image: menu_6,
+  },
+  {
+    menu_id: 7,
+    menu_name: "Italian",
+    menu_image: menu_7,
+  },
+  {
+    menu_id: 8,
+    menu_name: "Greek",
+    menu_image: menu_8,
+  },
+  {
+    menu_id: 9,
+    menu_name: "StuffedCrust",
+    menu_image: menu_9,
+  },
+];
+// const addToCart = (itemId) => {
+//   if (!cartItems[itemId]) {
+//     setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+//   } else {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+
+//     console.log(cartItems);
+//   }
+// };
